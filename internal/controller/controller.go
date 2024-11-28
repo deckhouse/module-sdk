31bb64da977f50184362f90a05dd3fa7c865a5f3
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-11-28 13:34:37 +0300

    bump queue name
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -165,7 +165,7 @@ func remapHookConfigToHookConfig(cfg *pkg.HookConfig) *hook.HookConfig {
 			AllowFailure:                 shcfg.AllowFailure,
 			ResynchronizationPeriod:      shcfg.ResynchronizationPeriod,
 			IncludeSnapshotsFrom:         shcfg.IncludeSnapshotsFrom,
-			Queue:                        cfg.Metadata.Path + cfg.Metadata.Name,
+			Queue:                        cfg.Queue,
 			Group:                        cfg.Metadata.Name,
 		}
 
