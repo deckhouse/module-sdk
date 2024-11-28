90fbec88629b8747972a9cdc81ff52a2c840b982
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-11-28 14:27:49 +0300

    move public func
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- internal/object-patch/object_filter.go
+++ internal/object-patch/object_filter.go
@@ -1,9 +1,7 @@
 package objectpatch
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 
 	pkgobjectpatch "github.com/deckhouse/module-sdk/pkg/object-patch"
 )
@@ -18,28 +16,4 @@ func (s Snapshots) GetObjectByKey(key string) ([]json.RawMessage, error) {
 	}
 
 	return snaps, nil
-}
-
-func UnmarshalToStruct[T any](s Snapshots, key string) ([]T, error) {
-	snaps, ok := s[key]
-	if !ok {
-		return nil, pkgobjectpatch.ErrSnapshotIsNotFound
-	}
-
-	result := make([]T, 0, len(snaps))
-
-	for _, snap := range snaps {
-		obj := new(T)
-
-		buf := bytes.NewBuffer(snap)
-
-		err := json.NewDecoder(buf).Decode(obj)
-		if err != nil {
-			return nil, fmt.Errorf("decode: %w", err)
-		}
-
-		result = append(result, *obj)
-	}
-
-	return result, nil
 }
--- pkg/object-patch/snapshots.go
+++ pkg/object-patch/snapshots.go
@@ -1,6 +1,13 @@
 package objectpatch
 
-import "errors"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/deckhouse/module-sdk/pkg"
+)
 
 var (
 	ErrSnapshotIsNotFound = errors.New("snapshot is not found")
@@ -12,4 +19,28 @@ func IgnoreSnapshotIsNotFound(err error) error {
 	}
 
 	return err
+}
+
+func UnmarshalToStruct[T any](s pkg.Snapshots, key string) ([]T, error) {
+	snaps, err := s.GetObjectByKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]T, 0, len(snaps))
+
+	for _, snap := range snaps {
+		obj := new(T)
+
+		buf := bytes.NewBuffer(snap)
+
+		err := json.NewDecoder(buf).Decode(obj)
+		if err != nil {
+			return nil, fmt.Errorf("decode: %w", err)
+		}
+
+		result = append(result, *obj)
+	}
+
+	return result, nil
 }
