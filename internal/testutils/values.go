f885004c0e9b9a664723fbe19940cb8daa296cc2
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-12-04 11:52:46 +0300

    remove rudiment code
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- internal/testutils/values.go
+++ internal/testutils/values.go
@@ -1,53 +1,8 @@
 package testutils
 
-import (
-	"fmt"
-
-	"github.com/davecgh/go-spew/spew"
-	k8syaml "sigs.k8s.io/yaml"
-)
-
 const (
 	GlobalValuesKey = "global"
 )
 
 // Values stores values for modules or hooks by name.
 type Values map[string]any
-
-// NewValuesFromBytes loads values sections from maps in yaml or json format
-func NewValuesFromBytes(data []byte) (Values, error) {
-	var values map[string]any
-
-	err := k8syaml.Unmarshal(data, &values)
-	if err != nil {
-		return nil, fmt.Errorf("bad values data: %s\n%s", err, string(data))
-	}
-
-	return values, nil
-}
-
-// NewValues load all sections from input data and makes sure that input map
-// can be marshaled to yaml and that yaml is compatible with json.
-func NewValues(data map[string]any) (Values, error) {
-	yamlDoc, err := k8syaml.Marshal(data)
-	if err != nil {
-		return nil, fmt.Errorf("data is not compatible with JSON and YAML: %s, data:\n%s", err, spew.Sdump(data))
-	}
-
-	var values Values
-	if err := k8syaml.Unmarshal(yamlDoc, &values); err != nil {
-		return nil, fmt.Errorf("convert data YAML to values: %s, data:\n%s", err, spew.Sdump(data))
-	}
-
-	return values, nil
-}
-
-func MergeValues(values ...Values) Values {
-	res := make(Values)
-
-	for _, v := range values {
-		res = MergeMap(res, v)
-	}
-
-	return res
-}
