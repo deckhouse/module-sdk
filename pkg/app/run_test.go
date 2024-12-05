d07104b6a13a084de450b696fdaefc0f5d9f0d69
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-12-05 13:58:57 +0300

    blackbox testing
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/app/run_test.go
+++ pkg/app/run_test.go
@@ -1,10 +1,11 @@
-package app
+package app_test
 
 import (
 	"bytes"
 	"testing"
 
 	"github.com/deckhouse/deckhouse/pkg/log"
+	"github.com/deckhouse/module-sdk/pkg/app"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,7 +16,7 @@ func Test_Run(t *testing.T) {
 
 	log.Default().SetOutput(buf)
 
-	Run()
+	app.Run()
 
 	assert.Contains(t, buf.String(), `{"level":"error","msg":"panic recover","panic":{"error":"failed to parse config: env: parse error on field \"CreateFilesByYourself\" of type \"bool\": strconv.ParseBool: parsing \"yes\": invalid syntax","stacktrace":`)
 }
