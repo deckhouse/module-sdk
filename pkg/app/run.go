75cc84aec9cc48394fdc9b154fd6798cab1d10e1
Pavel Okhlopkov <36456348+ldmonster@users.noreply.github.com>
2024-12-09 12:28:54 +0300

    handle panic (#2)
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>
    Co-authored-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/app/run.go
+++ pkg/app/run.go
@@ -1,21 +1,34 @@
 package app
 
 import (
+	"log/slog"
+	"runtime/debug"
+
 	"github.com/deckhouse/deckhouse/pkg/log"
 	"github.com/deckhouse/module-sdk/internal/controller"
 	"github.com/deckhouse/module-sdk/internal/transport/file"
 )
 
 func Run() {
+	logger := log.Default()
+
+	defer func() {
+		// recover from panic if one occurred. Set err to nil otherwise.
+		if r := recover(); r != nil {
+			panicLogger := logger.WithGroup("panic").
+				With(slog.Any("error", r)).
+				With(slog.String("stacktrace", string(debug.Stack())))
+			panicLogger.Error("panic recover")
+		}
+	}()
+
 	cfg := newConfig()
 	err := cfg.Parse()
 	if err != nil {
 		panic(err)
 	}
 
-	logger := log.NewLogger(log.Options{
-		Level: cfg.LogLevel.Level(),
-	})
+	logger.SetLevel(cfg.LogLevel)
 
 	fConfig := &file.Config{
 		BindingContextPath: cfg.HookConfig.BindingContextPath,
