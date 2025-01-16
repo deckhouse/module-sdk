dea5268386c00bdc57d909c837755cab56446832
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-01-16 13:28:43 +0300

    add hook
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- common-hooks/copy-custom-certificate/hook_test.go
+++ common-hooks/copy-custom-certificate/hook_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tlscertificate_test
+package copycustomcertificate_test
 
 import (
 	"bytes"
@@ -24,6 +24,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	copycustomcertificate "github.com/deckhouse/module-sdk/common-hooks/copy-custom-certificate"
 	tlscertificate "github.com/deckhouse/module-sdk/common-hooks/tls-certificate"
 	"github.com/deckhouse/module-sdk/pkg/certificate"
 	"github.com/deckhouse/module-sdk/pkg/jq"
@@ -47,19 +48,20 @@ func Test_JQFilterApplyCertificateSecret(t *testing.T) {
 	  "type": "kubernetes.io/tls"
 	}`
 
-		q, err := jq.NewQuery(tlscertificate.JQFilterApplyCertificateSecret)
+		q, err := jq.NewQuery(copycustomcertificate.JQFilterCustomCertificate)
 		assert.NoError(t, err)
 
 		res, err := q.FilterStringObject(context.Background(), rawSecret)
 		assert.NoError(t, err)
 
-		auth := new(certificate.Certificate)
-		err = json.NewDecoder(bytes.NewBufferString(res.String())).Decode(auth)
+		cert := new(certificate.Certificate)
+		err = json.NewDecoder(bytes.NewBufferString(res.String())).Decode(cert)
 		assert.NoError(t, err)
 
-		assert.Equal(t, "some-key", string(auth.Key))
-		assert.Equal(t, "some-crt", string(auth.Cert))
-		assert.Equal(t, "some-cert", auth.Name)
+		assert.Equal(t, "some-key", string(cert.Key))
+		assert.Equal(t, "some-crt", string(cert.Cert))
+		assert.Equal(t, "some-ca", string(cert.CA))
+		assert.Equal(t, "some-cert", cert.Name)
 	})
 
 	t.Run("apply tls from client", func(t *testing.T) {
@@ -85,12 +87,12 @@ func Test_JQFilterApplyCertificateSecret(t *testing.T) {
 		res, err := q.FilterStringObject(context.Background(), rawSecret)
 		assert.NoError(t, err)
 
-		cert := new(certificate.Certificate)
-		err = json.NewDecoder(bytes.NewBufferString(res.String())).Decode(cert)
+		auth := new(certificate.Certificate)
+		err = json.NewDecoder(bytes.NewBufferString(res.String())).Decode(auth)
 		assert.NoError(t, err)
 
-		assert.Equal(t, "some-key", string(cert.Key))
-		assert.Equal(t, "some-crt", string(cert.Cert))
-		assert.Equal(t, "some-cert", cert.Name)
+		assert.Equal(t, "some-key", string(auth.Key))
+		assert.Equal(t, "some-crt", string(auth.Cert))
+		assert.Equal(t, "some-cert", auth.Name)
 	})
 }
---
+++ pkg/utils/values-patch/values.go
@@ -0,0 +1,39 @@
+package valuespatch
+
+import (
+	"github.com/deckhouse/module-sdk/pkg"
+	"github.com/tidwall/gjson"
+)
+
+func getFirstDefined(values pkg.PatchableValuesCollector, keys ...string) (gjson.Result, bool) {
+	for i := range keys {
+		v, ok := values.GetOk(keys[i])
+		if ok {
+			return v, ok
+		}
+	}
+
+	return gjson.Result{}, false
+}
+
+func GetValuesFirstDefined(input *pkg.HookInput, keys ...string) (gjson.Result, bool) {
+	return getFirstDefined(input.Values, keys...)
+}
+
+func GetConfigValuesFirstDefined(input *pkg.HookInput, keys ...string) (gjson.Result, bool) {
+	return getFirstDefined(input.ConfigValues, keys...)
+}
+
+func GetHTTPSMode(input *pkg.HookInput, moduleName string) string {
+	var (
+		modulePath = moduleName + ".https.mode"
+		globalPath = "global.modules.https.mode"
+	)
+
+	v, ok := GetValuesFirstDefined(input, modulePath, globalPath)
+	if ok {
+		return v.String()
+	}
+
+	panic("https mode is not defined")
+}
