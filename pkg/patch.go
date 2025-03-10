b4acdeca4ff7d335d009078c2d00fabbf1dcaa60
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-03-10 14:20:30 +0300

    bump go, add comments
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/patch.go
+++ pkg/patch.go
@@ -17,12 +17,23 @@ type EMPatchableValuesCollector interface {
 }
 
 type PatchCollector interface {
+	// object must be Unstructured, map[string]any or runtime.Object
 	Create(object any)
+	// object must be Unstructured, map[string]any or runtime.Object
 	CreateIfNotExists(object any)
+	// object must be Unstructured, map[string]any or runtime.Object
 	CreateOrUpdate(object any)
 
+	// The object exists in the key-value store until the garbage collector
+	// deletes all the dependents whose ownerReference.blockOwnerDeletion=true
+	// from the key-value store.  API sever will put the "foregroundDeletion"
+	// finalizer on the object, and sets its deletionTimestamp.  This policy is
+	// cascading, i.e., the dependents will be deleted with Foreground.
 	Delete(apiVersion string, kind string, namespace string, name string)
+	// Deletes the object from the key-value store, the garbage collector will
+	// delete the dependents in the background.
 	DeleteInBackground(apiVersion string, kind string, namespace string, name string)
+	// Orphans the dependents.
 	DeleteNonCascading(apiVersion string, kind string, namespace string, name string)
 
 	// deprecated use PatchWithJSON instead
@@ -32,8 +43,15 @@ type PatchCollector interface {
 	// deprecated use PatchWithJQ instead
 	JQFilter(jqfilter string, apiVersion string, kind string, namespace string, name string, opts ...PatchCollectorOption)
 
+	// JSONPatch is a PatchType indicating the patch should be interpreted as a RFC6902 JSON Patch.
+	// This patch format requires specifying operations, paths, and values explicitly.
+	// See https://tools.ietf.org/html/rfc6902 for details.
 	PatchWithJSON(jsonPatch any, apiVersion string, kind string, namespace string, name string, opts ...PatchCollectorOption)
+	// MergePatch is a PatchType indicating the patch should be interpreted as a RFC7396 JSON Merge Patch.
+	// This patch format replaces elements at the object level rather than requiring explicit operations.
+	// See https://tools.ietf.org/html/rfc7396 for details.
 	PatchWithMerge(mergePatch any, apiVersion string, kind string, namespace string, name string, opts ...PatchCollectorOption)
+	// Mutate object with jq query
 	PatchWithJQ(jqfilter string, apiVersion string, kind string, namespace string, name string, opts ...PatchCollectorOption)
 
 	Operations() []PatchCollectorOperation
