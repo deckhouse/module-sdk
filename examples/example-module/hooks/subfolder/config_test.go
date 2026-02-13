b2855c7de86e8de8a8a343d4829a40d24c118526
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2026-02-13 17:18:47 +0300

    fix tests
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- examples/example-module/hooks/subfolder/config_test.go
+++ examples/example-module/hooks/subfolder/config_test.go
@@ -9,7 +9,7 @@ import (
 
 var _ = Describe("validate hooks config", func() {
 	It("hook configs must be valid", func() {
-		hooks := registry.Registry().Hooks()
+		hooks := registry.Registry().ModuleHooks()
 		for _, hook := range hooks {
 			Expect(hook.Config.Validate()).ShouldNot(HaveOccurred())
 		}
