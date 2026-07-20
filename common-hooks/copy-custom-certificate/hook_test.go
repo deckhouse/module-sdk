cac74b66e354a9ee620b1c93e1a3ba112fb10077
Denis Seryshev <108467057+diyliv@users.noreply.github.com>
2026-07-20 17:50:27 +0300

    [module-sdk] warn not error on missing certificate secret (#123)
    Signed-off-by: diyliv <onlogn081@gmail.com>

--- common-hooks/copy-custom-certificate/hook_test.go
+++ common-hooks/copy-custom-certificate/hook_test.go
@@ -22,10 +22,13 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	copycustomcertificate "github.com/deckhouse/module-sdk/common-hooks/copy-custom-certificate"
 	tlscertificate "github.com/deckhouse/module-sdk/common-hooks/tls-certificate"
+	"github.com/deckhouse/module-sdk/pkg"
 	"github.com/deckhouse/module-sdk/pkg/certificate"
+	"github.com/deckhouse/module-sdk/testing/framework"
 	"github.com/deckhouse/module-sdk/testing/helpers"
 )
 
@@ -91,4 +94,158 @@ func TestJQFilterApplyCertificateSecret_ParsesClientCertificate(t *testing.T) {
 	assert.Equal(t, "some-cert", auth.Name)
 	assert.Equal(t, "some-key", string(auth.Key))
 	assert.Equal(t, "some-crt", string(auth.Cert))
+}
+
+func TestHandler_SecretNotFound_ReturnsWarningNotError(t *testing.T) {
+	cfg := &pkg.HookConfig{
+		OnBeforeHelm: &pkg.OrderedConfig{Order: 10},
+		Kubernetes: []pkg.KubernetesConfig{
+			{
+				Name:       "custom_certificates",
+				APIVersion: "v1",
+				Kind:       "Secret",
+				NamespaceSelector: &pkg.NamespaceSelector{
+					NameSelector: &pkg.NameSelector{
+						MatchNames: []string{"d8-system"},
+					},
+				},
+				LabelSelector: &metav1.LabelSelector{
+					MatchExpressions: []metav1.LabelSelectorRequirement{
+						{
+							Key:      "owner",
+							Operator: metav1.LabelSelectorOpNotIn,
+							Values:   []string{"helm"},
+						},
+					},
+				},
+				JqFilter: copycustomcertificate.JQFilterCustomCertificate,
+			},
+		},
+	}
+
+	handler := copycustomcertificate.CopyCustomCertificatesHandler("testmodule")
+	hec := framework.HookExecutionConfigInit(t, cfg, handler, `{}`, `{}`)
+
+	hec.KubeStateSet(`
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: other-cert
+  namespace: d8-system
+  labels:
+    owner: deckhouse
+data:
+  tls.crt: Q1JUQ1JUQ1JU
+  tls.key: S0VZS0VZS0VZ
+type: kubernetes.io/tls
+`)
+
+	hec.ValuesSet("testmodule.https.mode", "CustomCertificate")
+	hec.ValuesSet("testmodule.https.customCertificate.secretName", "missing-cert")
+
+	hec.RunHook()
+
+	require.NoError(t, hec.HookError())
+
+	assert.Equal(t, "<none>", hec.ValuesGet("testmodule.internal.customCertificateData.ca\\.crt").String())
+	assert.Equal(t, "<none>", hec.ValuesGet("testmodule.internal.customCertificateData.tls\\.key").String())
+	assert.Equal(t, "<none>", hec.ValuesGet("testmodule.internal.customCertificateData.tls\\.crt").String())
+}
+
+func TestHandler_SecretExists_SetsValues(t *testing.T) {
+	cfg := &pkg.HookConfig{
+		OnBeforeHelm: &pkg.OrderedConfig{Order: 10},
+		Kubernetes: []pkg.KubernetesConfig{
+			{
+				Name:       "custom_certificates",
+				APIVersion: "v1",
+				Kind:       "Secret",
+				NamespaceSelector: &pkg.NamespaceSelector{
+					NameSelector: &pkg.NameSelector{
+						MatchNames: []string{"d8-system"},
+					},
+				},
+				LabelSelector: &metav1.LabelSelector{
+					MatchExpressions: []metav1.LabelSelectorRequirement{
+						{
+							Key:      "owner",
+							Operator: metav1.LabelSelectorOpNotIn,
+							Values:   []string{"helm"},
+						},
+					},
+				},
+				JqFilter: copycustomcertificate.JQFilterCustomCertificate,
+			},
+		},
+	}
+
+	handler := copycustomcertificate.CopyCustomCertificatesHandler("testmodule")
+	hec := framework.HookExecutionConfigInit(t, cfg, handler, `{}`, `{}`)
+
+	hec.KubeStateSet(`
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: my-cert
+  namespace: d8-system
+  labels:
+    owner: deckhouse
+data:
+  tls.crt: Q1JUQ1JUQ1JU
+  tls.key: S0VZS0VZS0VZ
+  ca.crt: Q0FDQUNBQ0E=
+type: kubernetes.io/tls
+`)
+
+	hec.ValuesSet("testmodule.https.mode", "CustomCertificate")
+	hec.ValuesSet("testmodule.https.customCertificate.secretName", "my-cert")
+
+	hec.RunHook()
+
+	require.NoError(t, hec.HookError())
+
+	assert.Equal(t, "CACACACA", hec.ValuesGet("testmodule.internal.customCertificateData.ca\\.crt").String())
+	assert.Equal(t, "KEYKEYKEY", hec.ValuesGet("testmodule.internal.customCertificateData.tls\\.key").String())
+	assert.Equal(t, "CRTCRTCRT", hec.ValuesGet("testmodule.internal.customCertificateData.tls\\.crt").String())
+}
+
+func TestHandler_NonCustomCertificateMode_RemovesValues(t *testing.T) {
+	cfg := &pkg.HookConfig{
+		OnBeforeHelm: &pkg.OrderedConfig{Order: 10},
+		Kubernetes: []pkg.KubernetesConfig{
+			{
+				Name:       "custom_certificates",
+				APIVersion: "v1",
+				Kind:       "Secret",
+				NamespaceSelector: &pkg.NamespaceSelector{
+					NameSelector: &pkg.NameSelector{
+						MatchNames: []string{"d8-system"},
+					},
+				},
+				LabelSelector: &metav1.LabelSelector{
+					MatchExpressions: []metav1.LabelSelectorRequirement{
+						{
+							Key:      "owner",
+							Operator: metav1.LabelSelectorOpNotIn,
+							Values:   []string{"helm"},
+						},
+					},
+				},
+				JqFilter: copycustomcertificate.JQFilterCustomCertificate,
+			},
+		},
+	}
+
+	handler := copycustomcertificate.CopyCustomCertificatesHandler("testmodule")
+	hec := framework.HookExecutionConfigInit(t, cfg, handler, `{}`, `{}`)
+
+	hec.KubeStateSet(``)
+	hec.ValuesSet("testmodule.https.mode", "CertManager")
+
+	hec.RunHook()
+
+	require.NoError(t, hec.HookError())
+	assert.False(t, hec.ValuesGet("testmodule.internal.customCertificateData").Exists())
 }
